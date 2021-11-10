29a092ffcc9e5f48170def4ecf668c7ee4840798
zaktitov <zaktitov@gmail.com>
2021-11-11 01:32:43 +0200

    colors fixed

--- src/js/api.js
+++ src/js/api.js
@@ -109,8 +109,21 @@ const options = {
   visiblePages: 5,
   centerAlign: true,
   template: {
+    page: '<a href="#" class="tui-page-btn" style="color:#ff6b01;  broder:1px solid transparent; border-radius:5px; width:40px; height:40px; display:inline-flex; align-items:center; justify-content:center;">{{page}}</a>',
     currentPage:
-      '<strong class="tui-page-btn tui-is-selected" style="background-color: #ff6b01; border-radius: 5px;">{{page}}</strong>',
+      '<strong class="tui-page-btn tui-is-selected" style="background-color: #ff6b01; broder:1px solid transparent;border-radius: 5px; font-size: 12px; width:40px; height:40px; display:inline-flex; align-items:center; justify-content:center;">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}"style="background-color:#F7F7F7; border-radius:5px; border:none;width:40px; height:40px; display:inline-flex; align-items:center; justify-content:center;">' +
+      '<span class="tui-ico-{{type}}" style="background-color:#F7F7F7; border:none">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}" style="background-color:#F7F7F7; border-radius:5px; width:40px; height:40px; display:inline-flex; align-items:center; justify-content:center;">' +
+      '<span class="tui-ico-{{type}}" style="color:#F7F7F7; border:none">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}" style="border-radius:5px;color:black;width:40px; height:40px; display:none; align-items:center; justify-content:center;">' +
+      '<span class="tui-ico-ellip" style="border:none;">...</span>' +
+      '</a>',
   },
 };
 
