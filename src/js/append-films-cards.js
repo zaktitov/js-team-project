35f870281101eae099090642e0b85e1688482b28
Dima-Shvets <81581083+Dima-Shvets@users.noreply.github.com>
2021-11-13 17:59:31 +0200

    create seperate append cards and reload page files

---
+++ src/js/append-films-cards.js
@@ -0,0 +1,17 @@
+// import moviesFromStorage from './get-movies-from-storage';
+import filmCardsTpl from '../templates/film-template.hbs';
+import FilmGenres from './film-genres';
+import getFilmFullYear from './film-full-year';
+import myCurrentPage from './currentPage';
+
+const filmGenres = new FilmGenres();
+
+
+
+export default async function (container, films) {
+  container.innerHTML = filmCardsTpl(films);
+  filmGenres.getFilmGenresList(container, '.js-film-genre');
+  myCurrentPage(films);
+  filmGenres.cutFilmGenres(container);
+  getFilmFullYear(container, '.js-film-release');
+}
