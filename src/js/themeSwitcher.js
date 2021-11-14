dd3cbbff83fa133cdb9ac65fdeb510489b767905
Elena Semeshyna <dp030492sos@gmail.com>
2021-11-14 11:18:42 +0100

    Add dark theme to modal window

--- src/js/themeSwitcher.js
+++ src/js/themeSwitcher.js
@@ -1,5 +1,7 @@
 const checkbox = document.getElementById('checkbox');
 const body = document.querySelector('body');
+const modalWindow = document.querySelector('.modal-js')
+
 
 const currentTheme = {
   DARK: 'dark-theme',
@@ -11,12 +13,18 @@ checkbox.addEventListener('change', changeTheme);
 function changeTheme(e) {
   if (checkbox.checked) {
     body.classList.toggle('dark');
+    modalWindow.classList.toggle('dark');
     localStorage.setItem('theme', currentTheme.DARK);
     body.classList.toggle('light');
+    modalWindow.classList.toggle('light');
+   
   } else {
     body.classList.toggle('dark');
+      modalWindow.classList.toggle('dark');
     localStorage.setItem('theme', currentTheme.LIGHT);
     body.classList.toggle('light');
+        modalWindow.classList.toggle('light');
+  
   }
 }
 
