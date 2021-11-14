4d2a2eb9d74673c23e5bcf6d3be6411c6fca6d43
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2021-11-14 16:36:58 +0100

    add flex to Scroll Btn

--- src/js/scroll-btn.js
+++ src/js/scroll-btn.js
@@ -1,14 +1,14 @@
 import { refs } from './refs.js';
-const { scrollBtn } = refs;
+const { scrollBtn, body } = refs;
 
 document.addEventListener('scroll', handleScroll);
 
 function handleScroll() {
-  var scrollableHeight =
+  const scrollableHeight =
     document.documentElement.scrollHeight - document.documentElement.clientHeight;
 
   if (document.documentElement.scrollTop / scrollableHeight > 0.3) {
-    if (!scrollBtn.classList.contains('show-scroll-btn'))
+    if (!scrollBtn.classList.contains('show-scroll-btn') && !body.classList.contains('is-open'))
       scrollBtn.classList.add('show-scroll-btn');
   } else {
     if (scrollBtn.classList.contains('show-scroll-btn'))
