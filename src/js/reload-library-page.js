3d82b8d3516e19e04ece7e0189b45572cb5db26d
Oleksandr Palatniy <oleksandr.palatniy@gmail.com>
2021-11-18 20:35:23 +0200

    add picture on empty card

--- src/js/reload-library-page.js
+++ src/js/reload-library-page.js
@@ -2,6 +2,8 @@ import { refs } from './refs';
 import appendFilmCardsMarkup from './append-films-cards';
 import moviesFromStorage from './get-movies-from-storage';
 
+import onEmptyLibrary from './header'
+
 const { getMoviesFromQueueStorage, getMoviesFromWatchedStorage } = moviesFromStorage;
 
 
@@ -12,8 +14,11 @@ export default function () {
   if (refs.headerQueueBtn.classList.contains('current')) {
     const films = getMoviesFromQueueStorage();
     appendFilmCardsMarkup(refs.libraryFilmCards, films);
+    
+    if (films.length === 0) {onEmptyLibrary()}
     return
   }
   const films = getMoviesFromWatchedStorage();
   appendFilmCardsMarkup(refs.libraryFilmCards, films);
+  if (films.length === 0) {onEmptyLibrary()}
 }
